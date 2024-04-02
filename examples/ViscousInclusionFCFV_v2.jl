821cab20da3375d7bb49d47b984654ecfda31c26
tduretz <duretz@uni-frankfurt.de>
2024-04-02 16:51:46 +0200

    update viscous inclusion test

--- examples/ViscousInclusionFCFV_v2.jl
+++ examples/ViscousInclusionFCFV_v2.jl
@@ -66,19 +66,19 @@ function ViscousInclusion()
     xmin, xmax  = -3.0, 3.0    # Domain extent x
     ymin, ymax  = -3.0, 3.0    # Domain extent y
     R           = 1.0          # Inclusion radius
-    η           = [1.0 1e-2]   # Viscosity matrix/inclusion
+    η           = [1.0 1e+2]   # Viscosity matrix/inclusion
     BC          = [2; 1; 1; 1] # South/East/North/West --- 1: Dirichlet / 2: Neumann
 
     # Numerics
     Mesher      = :Delaunay                # :Delaunay / :AdvancingFront (load external mesh)
     mesh_res    = :MedRes                  # :LowRes / :MedRes / :HighRes     
-    solver      = :PowellHestenesLU        # :CoupledBackslash / :=PowellHestenesCholesky / :=PowellHestenesLU
+    solver      = :PowellHestenesCholesky  # :CoupledBackslash / :=PowellHestenesCholesky / :=PowellHestenesLU
     Formulation = :SymmetricGradient       # :Gradient / :SymmetricGradient
-    τr          = 2                        # Stabilisation
+    τr          = 2.                        # Stabilisation
     γ           = 1e5                      # Penalty factor for Powell-Hestenes solvers
-    ϵ           = 1e-8                     # Tolerance of Powell-Hestenes solvers 
+    ϵ           = 1e-8 
 
-    # Generate mesh 
+       # Generate mesh 
     nx, ny = 60, 60  # initial point density in x and y for triangulation 
     if Mesher==:Delaunay       mesh = MakeTriangleMesh( nx, ny, xmin, xmax, ymin, ymax, τr, 1, R, BC, ((xmax-xmin)/nx)*((ymax-ymin)/ny), 200 ) end
     if Mesher==:AdvancingFront mesh = LoadExternalMesh( mesh_res, η) end
@@ -90,7 +90,7 @@ function ViscousInclusion()
     if Formulation==:Gradient && solver==:PowellHestenesCholesky
         error("Gradient formulation leads to non symmetric system, can't apply Cholesky factorisation!")
     end
-    
+
     # Source term and BCs etc...
     Vxh    = zeros(mesh.nf)   # Velocity x (hybrid variable)
     Vyh    = zeros(mesh.nf)   # Velocity y (hybrid variable)
@@ -122,15 +122,15 @@ function ViscousInclusion()
     # Set up Stokes problem
     @printf("---> Model configuration :\n")
     @time SetUpProblem!(mesh, Pa, Vxa, Vya, σxxa, σyya, σxya, VxDir, VyDir, σxxNeu, σyyNeu, σxyNeu, σyxNeu, sex, sey, R, η, Formulation, ηip, N, nnel, nip)
-
+        
     # Compute mesh properties for FCFV
     @printf("---> Compute FCFV vectors:\n")
-    mesh.τe .= τr.*ones(mesh.nel)  # Stabilisation per element
+    mesh.τe = τr.*ones(mesh.nf)  # Stabilisation per element
     @time ae, be, ze = ComputeFCFV(mesh, sex, sey, VxDir, VyDir, σxxNeu, σyyNeu, σxyNeu, σyxNeu, τr, Formulation)
     
     # Assemble element matrices and RHS
-    @printf("---> Compute element matrices:\n")  
-    @time Kuu, Muu, Kup, fu, fp, tsparse = ElementAssemblyLoopToy(mesh, ae, be, ze, VxDir, VyDir, σxxNeu, σyyNeu, σxyNeu, σyxNeu, Formulation)
+    @printf("---> Compute element matrices:\n")                      
+    @time Kuu, Muu, Kup, fu, fp, tsparse = ElementAssemblyLoop(mesh, ae, be, ze, VxDir, VyDir, σxxNeu, σyyNeu, σxyNeu, σyxNeu, Formulation)
     println("---> Sparsification: ", tsparse)
 
     # # Solve for hybrid variables
@@ -145,7 +145,7 @@ function ViscousInclusion()
     # Evaluate residuals
     @printf("---> Evaluate residuals:\n")
     ComputeResidualsFCFV_Stokes_o1(Vxh, Vyh, Pe, mesh, ae, be, ze, sex, sey, VxDir, VyDir, σxxNeu, σyyNeu, σxyNeu, σyxNeu, Formulation)
-
+    
     # Visualise
     @printf("---> Visualisation:\n")
     # @time PlotMakie( mesh, Vxe,  xmin, xmax, ymin, ymax; cmap=:turbo, min_v=-3, max_v=3, writefig=false )
